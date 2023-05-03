676e805ad00cb07820b7c051bb97a4f8a5abf26b
Vendula Krajíčková <vendysacek@seznam.cz>
2023-05-03 20:33:36 +0200

    Final version

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // console.log('funguju!');
 
 const renderTemplate = (items, icon) => {
-<<<<<<< HEAD
     let emailIcon = '';
 
     if (icon === 'read') {
@@ -32,40 +31,6 @@ const renderTemplate = (items, icon) => {
 
 }
 
-=======
-    let element, emailIcon = '';
-
-    if (icon === 'read') {
-        element = document.querySelector('.read');
-        emailIcon = `<button class="email__icon email__icon--opened"></button>`;
-    }
-
-    if (icon === 'unread') {
-        element = document.querySelector('.unread');
-        emailIcon = `<button class="email__icon email__icon--closed"></button>`;
-    }
-
-    element.innerHTML = items
-        .map(oneEmail => `
-            <div class="email">
-                <div class="email__head">
-                ${emailIcon}
-                <div class="email__info">
-                    <div class="email__sender">${oneEmail.sender.name}</div>
-                    <div class="email__subject">${oneEmail.subject}</div>
-                </div>
-                <div class="email__time">${oneEmail.time}</div>
-                </div>
-                <div class="email__body"></div>
-            </div>
-        `)
-        .join('');
-
-}
-
-// fetch('https://apps.kodim.cz/daweb/trening-api/apis/emails?folder=all')
-
->>>>>>> e89f1f85ac7812e8f9127377b414489582876d9e
 fetch('https://apps.kodim.cz/daweb/trening-api/apis/emails?folder=read')
     .then((response) => response.json())
     .then((data) => {
