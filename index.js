c4b214ba89ae25e09ed66754685eb7ed2fc27b28
Vendula Krajíčková <vendysacek@seznam.cz>
2023-05-03 20:02:38 +0200

    Finální verze

--- index.js
+++ index.js
@@ -1,51 +1,44 @@
-console.log('funguju!');
+// console.log('funguju!');
 
-const renderRead = (items) => {
-    document.querySelector('.read').innerHTML = items
-    .map(oneEmail => `
-        <div class="email">
-            <div class="email__head">
-            <button class="email__icon email__icon--opened"></button>
-            <div class="email__info">
-                <div class="email__sender">${oneEmail.sender.name}</div>
-                <div class="email__subject">${oneEmail.subject}</div>
+const renderTemplate = (items, icon) => {
+    let emailIcon = '';
+
+    if (icon === 'read') {
+        emailIcon = `opened`;
+    }
+
+    if (icon === 'unread') {
+        emailIcon = `closed`;
+    }
+
+    const item = items
+        .map(oneEmail => `
+            <div class="email">
+                <div class="email__head">
+                <button class="email__icon email__icon--${emailIcon}"></button>
+                <div class="email__info">
+                    <div class="email__sender">${oneEmail.sender.name}</div>
+                    <div class="email__subject">${oneEmail.subject}</div>
+                </div>
+                <div class="email__time">${oneEmail.time}</div>
+                </div>
+                <div class="email__body"></div>
             </div>
-            <div class="email__time">${oneEmail.time}</div>
-            </div>
-            <div class="email__body"></div>
-        </div>
-    `)
-    .join('');
+        `)
+        .join('');
+
+    document.querySelector(`#${icon}`).insertAdjacentHTML('afterend', item);
+
 }
-
-const renderUnread = (items) => {
-    document.querySelector('.unread').innerHTML = items
-    .map(oneEmail => `
-        <div class="email">
-            <div class="email__head">
-            <button class="email__icon email__icon--closed"></button>
-            <div class="email__info">
-                <div class="email__sender">${oneEmail.sender.name}</div>
-                <div class="email__subject">${oneEmail.subject}</div>
-            </div>
-            <div class="email__time">${oneEmail.time}</div>
-            </div>
-            <div class="email__body"></div>
-        </div>
-    `)
-    .join('');
-}
-
-// fetch('https://apps.kodim.cz/daweb/trening-api/apis/emails?folder=all')
 
 fetch('https://apps.kodim.cz/daweb/trening-api/apis/emails?folder=read')
     .then((response) => response.json())
     .then((data) => {
-        renderRead(data.emails);
+        renderTemplate(data.emails, "read");
     })
 
 fetch('https://apps.kodim.cz/daweb/trening-api/apis/emails?folder=unread')
     .then((response) => response.json())
     .then((data) => {
-        renderUnread(data.emails);
+        renderTemplate(data.emails, "unread");
     })
